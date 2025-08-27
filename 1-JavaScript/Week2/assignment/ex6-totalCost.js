af92555ac3559438fcb2075cd7f9406130694822
Rim Hadid <105562650+Hadidreem17@users.noreply.github.com>
2025-08-27 19:16:18 +0200

    Update ex6-totalCost.js

--- 1-JavaScript/Week2/assignment/ex6-totalCost.js
+++ 1-JavaScript/Week2/assignment/ex6-totalCost.js
@@ -20,22 +20,29 @@ instead!
 3. Complete the unit test functions and verify that all is working as expected.
 -----------------------------------------------------------------------------*/
 const cartForParty = {
-  // TODO complete this object
+  chips: 2.2,
+  soda: 1.4,
+  beers: 3.5,
+  cookies: 2.0,
+  pizza: 7.5
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
-  // TODO replace this comment with your code
+function calculateTotalPrice(cart) {
+  const values = Object.values(cart);
+  const sum = values.reduce((acc, num) => acc + num, 0);
+  return `Total: €${sum}`;
 }
 
-// ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
-  // TODO replace this comment with your code
+  console.assert(calculateTotalPrice.length === 1);
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
-  // TODO replace this comment with your code
+  const expected = 'Total: €16.6';
+  const actual = calculateTotalPrice(cartForParty);
+  console.assert(actual === expected);
 }
 
 function test() {
