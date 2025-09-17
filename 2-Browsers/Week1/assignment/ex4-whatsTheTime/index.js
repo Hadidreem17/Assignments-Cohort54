345cd8ea3b8029682dc7ae49d1208aa90c9bfa0e
Hadidreem17 <Hadidreem17@gmail.com>
2025-09-17 22:03:43 +0200

    Week1 Browsers: solve ex4-whatsTheTime

--- 2-Browsers/Week1/assignment/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/assignment/ex4-whatsTheTime/index.js
@@ -7,7 +7,8 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-B
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
 function addCurrentTime() {
-  // TODO complete this function
-}
+  document.querySelector('time').textContent= new Date().toLocaleTimeString() ;
+  setInterval(addCurrentTime, 1000) ;
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+}
+window.onload = addCurrentTime;
