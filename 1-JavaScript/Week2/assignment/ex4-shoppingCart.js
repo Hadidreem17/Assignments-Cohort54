2223d933a59b4226f5f5962abf556155da236d70
Hadidreem17 <Hadidreem17@gmail.com>
2025-09-21 21:16:57 +0200

    Fix Week2 ex4-shoppingCart based on feedback

--- 1-JavaScript/Week2/assignment/ex4-shoppingCart.js
+++ 1-JavaScript/Week2/assignment/ex4-shoppingCart.js
@@ -1,29 +1,57 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/1-JavaScript/Week3#exercise-4-shopping-at-the-supermarket
+const shoppingCart = ['bananas', 'milk'];
 
-Let's do some grocery shopping! We're going to get some things to cook dinner
-with. However, you like to spend money and always buy too many things. So when 
-you have more than 3 items in your shopping cart the first item gets taken out.
-
-const shoppingCart = ["bananas", "milk"];
-
-export function addToShoppingCart(item) {
+function addToShoppingCart(item) {
   if (item !== undefined) {
     shoppingCart.push(item);
     if (shoppingCart.length > 3) {
       shoppingCart.shift();
     }
   }
-  return `You bought ${shoppingCart.join(", ")}!`;
+  return `You bought ${shoppingCart.join(', ')}!`;
 }
 
-function main() {
-  console.log(addToShoppingCart());
-  console.log(addToShoppingCart("chocolate"));
-  console.log(addToShoppingCart("waffles"));
-  console.log(addToShoppingCart("tea"));
+// ===== manual tests (restore) =====
+function test1() {
+  console.log('Test 1: add `chocolate` to the cart');
+  const expected = 'You bought bananas, milk, chocolate!';
+  const actual = addToShoppingCart('chocolate');
+  console.assert(actual === expected);
 }
 
-if (process.env.NODE_ENV !== "test") {
-  main();
+function test2() {
+  console.log('Test 2: add `waffles` (keep last 3 items)');
+  const expected = 'You bought milk, chocolate, waffles!';
+  const actual = addToShoppingCart('waffles');
+  console.assert(actual === expected);
 }
+
+function test3() {
+  console.log('Test 3: add `tea` (bananas removed)');
+  const expected = 'You bought chocolate, waffles, tea!';
+  const actual = addToShoppingCart('tea');
+  console.assert(actual === expected);
+}
+
+function test4() {
+  console.log('Test 4: add nothing (cart unchanged)');
+  const expected = 'You bought chocolate, waffles, tea!';
+  const actual = addToShoppingCart();
+  console.assert(actual === expected);
+}
+
+function test5() {
+  console.log('Test 5: `tea` should be added and `milk` removed');
+  const expected = 'You bought chocolate, waffles, tea!';
+  const actual = addToShoppingCart('tea');
+  console.assert(actual === expected);
+}
+
+function test() {
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+}
+
+test();
