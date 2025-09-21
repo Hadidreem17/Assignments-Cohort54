84f2263e9a17390a0385c454dd29cbfb9d9fab90
Hadidreem17 <Hadidreem17@gmail.com>
2025-09-21 20:55:52 +0200

    Week2: fix solution for ex3-tellFortune

--- 1-JavaScript/Week2/assignment/ex3-tellFortune.js
+++ 1-JavaScript/Week2/assignment/ex3-tellFortune.js
@@ -1,22 +1,34 @@
-export function tellFortune() {
-  const numKids = [0, 1, 2, 3, 4];
-  const partnerNames = ["Alex", "Sam", "Taylor", "Jordan", "Casey"];
-  const locations = ["Amsterdam", "Rotterdam", "Utrecht", "The Hague", "Eindhoven"];
-  const jobs = ["developer", "designer", "teacher", "engineer", "chef"];
 
-  const pick = (arr) => arr[Math.floor(Math.random() * arr.length)];
+function selectRandomly(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-  return `You will be a ${pick(jobs)} in ${pick(locations)}, and married to ${pick(partnerNames)} with ${pick(numKids)} kids.`;
+export function tellFortune(numKids, partnerNames, locations, jobTitles) {
+  return `You will be a ${selectRandomly(jobTitles)} in ${selectRandomly(
+    locations
+  )}, married to ${selectRandomly(partnerNames)} with ${selectRandomly(
+    numKids
+  )} kids.`;
 }
 
 function main() {
-  console.log(tellFortune());
-  console.log(tellFortune());
-  console.log(tellFortune());
+  const numKids = [0, 1, 2, 3, 4];
+  const partnerNames = ["Alex", "Sam", "Taylor", "Jordan", "Casey"];
+  const locations = ["Amsterdam", "Rotterdam", "Utrecht", "The Hague", "Eindhoven"];
+  const jobTitles = ["developer", "designer", "teacher", "engineer", "chef"];
+
+  console.log(tellFortune(numKids, partnerNames, locations, jobTitles));
+  console.log(tellFortune(numKids, partnerNames, locations, jobTitles));
+  console.log(tellFortune(numKids, partnerNames, locations, jobTitles));
 }
 
-if (process.env.NODE_ENV !== "test") {
+// ! Do not change or remove the code below
+if (process.env.NODE_ENV !== 'test') {
   main();
 }
+
+
+
+
 
 
